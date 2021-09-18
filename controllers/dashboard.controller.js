ca9a1fc46844dad77dba626111c97132195c8ba9
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2021-09-18 14:31:15 +0500

    fixed dashboard api

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
 const _ = require('lodash');
 const Purchase = require('../models/v2/purchases.model').Model;
-const Model = require('../models/v2/customers.model');
 const Sale = require('../models/v2/sales.model');
-const Type = require('../models/v2/types.model');
 const Expense = require('../models/v2/expenses.model');
 const Salary = require('../models/v2/salaries.model');
 
 const { catchAsync } = require('./errors.controller');
-const AppError = require('../utils/AppError');
 
 module.exports.getPurchases = catchAsync(async function (req, res, next) {
     const { startDate, endDate } = req.query;
 
+    console.log(await Purchase.find(), startDate, endDate);
+
     const results = await Purchase.find({ createdAt: { $gte: startDate, $lte: endDate } }).lean();
-    const sum = results.map((item) => item.totalSourcePrice).reduce((prev, curr) => prev + curr, 0);
+    const prices = results.map((item) => item.totalSourcePrice);
+    let sum = 0;
+    if (prices.length > 1) sum = prices.reduce((prev, curr) => prev + curr, 0);
+    else if (prices.length === 1) sum = prices[0];
 
     res.status(200).send({
         sum,
@@ -26,7 +27,11 @@ module.exports.getSales = catchAsync(async function (req, res, next) {
     const { startDate, endDate } = req.query;
 
     const results = await Sale.find({ createdAt: { $gte: startDate, $lte: endDate } }).lean();
-    const sum = results.map((item) => item.totalRetailPrice).reduce((prev, curr) => prev + curr, 0);
+
+    const prices = results.map((item) => item.totalRetailPrice);
+    let sum = 0;
+    if (prices.length > 1) sum = prices.reduce((prev, curr) => prev + curr, 0);
+    else if (prices.length === 1) sum = prices[0];
 
     res.status(200).send({
         sum,
@@ -60,10 +65,12 @@ module.exports.getRevenue = catchAsync(async function (req, res, next) {
 });
 
 async function getExpenses(startDate, endDate) {
-    const expenses = await Expense.find({ createdAt: { $gte: startDate, $lte: endDate } }).lean();
+    const [expenses, salaries] = await Promise.all([
+        Expense.find({ createdAt: { $gte: startDate, $lte: endDate } }).lean(),
+        Salary.find({ createdAt: { $gte: startDate, $lte: endDate } }).lean(),
+    ]);
     const totalExpenses = expenses.map((item) => item.amount).reduce((prev, curr) => prev + curr, 0);
 
-    const salaries = await Salary.find({ createdAt: { $gte: startDate, $lte: endDate } }).lean();
     const totalSalaries = salaries.map((item) => item.amount).reduce((prev, curr) => prev + curr, 0);
 
     return totalExpenses + totalSalaries;
@@ -84,76 +91,4 @@ module.exports.getExpenses = catchAsync(async function (req, res, next) {
 
     const amount = await getExpenses(startDate, endDate);
     res.status(200).json(amount);
-});
-
-module.exports.getOne = catchAsync(async function (req, res, next) {
-    const { id } = req.params;
-    const { page, limit, sort } = req.query;
-
-    if (!mongoose.isValidObjectId(id)) return next(new AppError('Please enter a valid id', 400));
-
-    const customer = await Model.findById(id, { __v: 0 }).lean();
-
-    if (!customer) return next(new AppError('Customer does not exist', 404));
-
-    const sales = await Sale.paginate(
-        { customer: mongoose.Types.ObjectId(id) },
-        {
-            populate: { path: 'customer', select: '-__v' },
-            projection: { __v: 0 },
-            lean: true,
-            page,
-            limit,
-            sort,
-        }
-    );
-
-    res.status(200).json({
-        customer,
-        sales: _.omit(sales, ['page', 'prevPage', 'nextPage', 'limit']),
-    });
-});
-
-module.exports.addMany = catchAsync(async function (req, res, next) {
-    const docs = req.body;
-
-    if (!docs || !docs.length) return next(new AppError('Please enter valid customers', 400));
-
-    await Model.insertMany(docs);
-
-    res.status(200).json();
-});
-
-module.exports.addOne = catchAsync(async function (req, res, next) {
-    const newDoc = _.pick(req.body, ['name', 'phone']);
-    await Model.create(newDoc);
-    res.status(200).send();
-});
-
-module.exports.edit = catchAsync(async function (req, res, next) {
-    const { id } = req.params;
-
-    if (!mongoose.isValidObjectId(id)) return next(new AppError('Please enter a valid id', 400));
-
-    const newDoc = _.pick(req.body, ['name', 'phone']);
-
-    if (!Object.keys(newDoc).length) return next(new AppError('Please enter a valid customer', 400));
-
-    await Model.updateOne({ _id: id }, newDoc, { runValidators: true });
-
-    res.status(200).json();
-});
-
-module.exports.remove = catchAsync(async function (req, res, next) {
-    let ids = req.params.id.split(',');
-
-    for (const id of ids) {
-        if (!mongoose.isValidObjectId(id)) return next(new AppError('Please enter valid id(s)', 400));
-    }
-
-    ids = ids.map((id) => mongoose.Types.ObjectId(id));
-
-    await Model.deleteMany({ _id: { $in: ids } });
-
-    res.status(200).json();
 });
--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const autoParams = require('../utils/autoParams');
 
-const { getAll, addOne, edit, remove, getOne } = require('../controllers/v2/customers.controller');
 const { getPurchases, getSales, getRevenue, getProfit, getExpenses } = require('../controllers/dashboard.controller');
 
 router.get('/purchases', autoParams, getPurchases);
@@ -9,11 +8,5 @@ router.get('/sales', autoParams, getSales);
 router.get('/revenue', autoParams, getRevenue);
 router.get('/expenses', autoParams, getExpenses);
 router.get('/profit', autoParams, getProfit);
-
-router.get('/', autoParams, getAll);
-router.get('/id/:id', autoParams, getOne);
-router.route('/').post(addOne);
-router.route('/id/:id').patch(edit);
-router.route('/id/:id').delete(remove);
 
 module.exports = router;
